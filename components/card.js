362237534265079436327c883772643f73904393
FrancoisS <f.soler1995@gmail.com>
2022-11-18 00:54:40 +0100

    fixed img

--- components/card.js
+++ components/card.js
@@ -4,22 +4,23 @@ import NextImage from "./image"
 
 const Card = ({ article }) => {
   return (
-    <Link className="uk-link-reset" href={`/article/${article.attributes.slug}`}>
-       
-        <div className="uk-card uk-card-muted">
-          <div className="uk-card-media-top">
-            <NextImage image={article.attributes.image} />
-          </div>
-          <div className="uk-card-body">
-            <p id="category" className="uk-text-uppercase">
-              {article.attributes.category.name}
-            </p>
-            <p id="title" className="uk-text-large">
-              {article.attributes.title}
-            </p>
-          </div>
+    <Link
+      className="uk-link-reset"
+      href={`/article/${article.attributes.slug}`}
+    >
+      <div className="uk-card uk-card-muted">
+        <div className="uk-card-media-top">
+          <NextImage image={article.attributes.image} />
         </div>
-      
+        <div className="uk-card-body">
+          <p id="category" className="uk-text-uppercase">
+            {article.attributes.category.name}
+          </p>
+          <p id="title" className="uk-text-large">
+            {article.attributes.title}
+          </p>
+        </div>
+      </div>
     </Link>
   )
 }
--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -5,10 +5,15 @@ import Layout from "../../components/layout"
 import NextImage from "../../components/image"
 import Seo from "../../components/seo"
 import { getStrapiMedia } from "../../lib/media"
-
-const Article = ({ article, categories }) => {
+import Image from "next/image"
+const Article = ({ article, categories, author }) => {
   const imageUrl = getStrapiMedia(article.attributes.image)
-console.log('articles', article.attributes)
+  console.log("articles", article.attributes)
+  const filteredAuthor = author.data.find(
+    (x) => x.id === article.attributes.author.data.id
+  )
+  console.log("writers", filteredAuthor.attributes)
+
   const seo = {
     metaTitle: article.attributes.title,
     metaDescription: article.attributes.description,
@@ -30,16 +35,25 @@ console.log('articles', article.attributes)
       </div>
       <div className="uk-section">
         <div className="uk-container uk-container-small">
-          <ReactMarkdown
-            children={article.attributes.content}
-            
-          />
+          <ReactMarkdown children={article.attributes.content} />
           <hr className="uk-divider-small" />
           <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
             <div>
-              {article.attributes.author.picture && (
-                <NextImage image={article.attributes.author.data.attributes.picture} />
-              )}
+              {/* {filteredAuthor.data && ( */}
+              <img
+                image={
+                  "http://localhost:3000"+filteredAuthor.attributes.picture.data.attributes.formats
+                  .medium.url
+                }
+                width="100"
+                height="100"
+                placeholder="blur"
+                src={
+                  "http://localhost:1337"+filteredAuthor.attributes.picture.data.attributes.formats
+                  .medium.url
+                }
+              />
+              {/*    )} */}
             </div>
             <div className="uk-width-expand">
               <p className="uk-margin-remove-bottom text-3xl text-purple-5">
@@ -78,10 +92,21 @@ export async function getStaticProps({ params }) {
     },
     populate: "*",
   })
+  console.log("hey")
+  const writersRes = await fetchAPI("/writers", {
+    filters: {
+      id: articlesRes.data[0].author,
+    },
+    populate: "*",
+  })
   const categoriesRes = await fetchAPI("/categories")
 
   return {
-    props: { article: articlesRes.data[0], categories: categoriesRes },
+    props: {
+      article: articlesRes.data[0],
+      categories: categoriesRes,
+      author: writersRes,
+    },
     revalidate: 1,
   }
 }
